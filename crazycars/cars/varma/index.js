f401f668a16fe7a1f281d0e2878ae34011ae1e94
dumpalakomalkiran <dumpalakomalkiran@gmail.com>
2016-09-16 07:01:30 +0530

    crazycars

---
+++ crazycars/cars/varma/index.js
@@ -0,0 +1,27 @@
+var data = {
+	one: {
+		id: 1,
+		name: 'Hiro Protagonist',
+		age: 27
+	},
+	two: {
+		id: 2,
+		name: 'Y.T.',
+		age: 16
+	},
+	three: {
+		id: 3,
+		name: 'Raven',
+		age: 40,
+	},
+	four: {
+		id: 4,
+		name: 'Uncle Enzo',
+		age: 80
+	},
+	five: {
+		id: 5,
+		name: 'Fisheye',
+		age: 50
+	}
+};
